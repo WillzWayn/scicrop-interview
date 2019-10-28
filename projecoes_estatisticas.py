17dad519d4f990b132000b7386712cddf728ff05
William Wayn <willzwayn@gmail.com>
2019-10-28 19:15:57 -0300

    Add files via upload
    Otimização do arquivo projeções.py.
    o antiga estava desnecessariamente  carregando gráficos que custavam para ser plotados.

--- projecoes_estatisticas.py
+++ projecoes_estatisticas.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[12]:
+# In[ ]:
 
 
 # Import modules
 import numpy as np # Numeric operations
 import pandas as pd # Data manipulation
-import matplotlib.pyplot as plt # Plots
-import seaborn as sns
 import pickle
 
 import warnings
@@ -19,7 +17,7 @@ warnings.filterwarnings("ignore")
 path='WEB MachineLearning model deployed with Flask/static/modelos/'
 
 
-# In[13]:
+# In[ ]:
 
 
 # Read the initial dataset
@@ -31,61 +29,69 @@ production on various location acquired from the Indian government web page.
 https://data.gov.in""";
 
 
-# In[14]:
+# In[ ]:
 
 
 # A Look in the dataframe
 apy.head()
 
 
-# In[15]:
+# In[ ]:
 
 
 # Check Shape
 apy.shape
 
 
-# In[16]:
+# In[ ]:
 
 
 # Check Variable types
 apy.dtypes
 
+# Production tem problema !
 
-# In[17]:
+
+# In[ ]:
 
 
 # check for null values
 apy.isnull().sum()
 
 
-# In[18]:
+# In[ ]:
+
+
+apy[apy['Production'] == '='].head(2)
+
+
+# In[ ]:
 
 
 #Years in apy dataset
 #sorted((apy.Crop_Year.value_counts().index))
 
 
-# In[19]:
+# In[ ]:
 
 
 gdp = pd.read_csv('API_NY.GDP.MKTP.CD_DS2_en_csv_v2_247793.csv', skiprows=4)
 
 
-# In[20]:
+# In[ ]:
 
 
 gdp.head(3)
 
 
-# In[21]:
+# In[ ]:
 
 
 # Locate India
 gdp.loc[gdp['Country Name'] == 'India']
 
 
-# In[22]:
+# In[ ]:
 
 
 # Select only 1997-2015
@@ -95,7 +101,7 @@ end_year = gdp.columns.get_loc('2015')
 gdp_9715 = gdp.iloc[107, start_year:end_year+1]
 
 
-# In[23]:
+# In[ ]:
 
 
 # Create a Dataframe for gdp
@@ -106,7 +112,7 @@ gdp_df = gdp_df.reset_index(drop=True)
 gdp_df.head()
 
 
-# In[24]:
+# In[ ]:
 
 
 # Check if Crop_Year have all values from 1997 to 2015
@@ -114,21 +120,21 @@ apy_crop_year = apy['Crop_Year']
 apy_crop_year.unique()
 
 
-# In[25]:
+# In[ ]:
 
 
 # Check Datatypes
 gdp_df.dtypes
 
 
-# In[26]:
+# In[ ]:
 
 
 # Convert year to int
 gdp_df['Year'] = gdp_df['Year'].astype(int)
 
 
-# In[27]:
+# In[ ]:
 
 
 # Map all GDP values to the respective year
@@ -139,42 +145,42 @@ apy_crop_year_dict = {'GDP': apy_crop_year}
 gdp_final = pd.DataFrame(apy_crop_year_dict)
 
 
-# In[28]:
+# In[ ]:
 
 
 # See the data type
 gdp_final['GDP'].dtype
 
 
-# In[29]:
+# In[ ]:
 
 
 # Concatenate the DataFrames
 india_crop_gdp_1997_2015 = pd.concat([apy, gdp_final], axis=1, sort=False)
 
 
-# In[30]:
+# In[ ]:
 
 
 # A look into DataFrame
 india_crop_gdp_1997_2015.head()
 
 
-# In[31]:
+# In[ ]:
 
 
 # Save as .csv
 india_crop_gdp_1997_2015.to_csv('india_crop_gdp_1997_2015.csv')
 
 
-# In[32]:
+# In[ ]:
 
 
 india_crop_gdp_1997_2015= pd.read_csv('india_crop_gdp_1997_2015.csv')
 india_crop_gdp_1997_2015.columns
 
 
-# In[33]:
+# In[ ]:
 
 
 # Label Enconding
@@ -184,30 +190,37 @@ for f in india_crop_gdp_1997_2015.columns:
     if f == 'Production':
         continue
     if india_crop_gdp_1997_2015[f].dtype =='object': 
-        vet = []
         le = preprocessing.LabelEncoder()
         itens = (sorted(list(india_crop_gdp_1997_2015[f].value_counts().index)))
         le.fit(list(india_crop_gdp_1997_2015[f].values))
         itens_edited = le.transform(itens)
-        vet.append([[a,b] for a,b in zip (itens,itens_edited)])
+        RelationItens[f]={b:a for a,b in zip (itens,itens_edited)}
         india_crop_gdp_1997_2015[f] = le.transform(list(india_crop_gdp_1997_2015[f].values))
-        RelationItens[f]=vet
 #del RelationItens['Production']
 RelationItens.keys()
 
 
-# In[42]:
+# In[ ]:
 
 
-#for i in RelationItens['Season'][0]:
+#for i in RelationItens['Season']:
 #    print ('        <option value="{}">{}</option>'.format(i[1],i[0]))
 
 
-# # Now we can train the data!
+# # Separação do data set
 # 
-# ## For the first algorithm (Classification problem)
+# ## Usaremos 2 datasets para aplicar a pesquisa! O que acontece é que a coluna de Produção apresenta problemas em alguns valores. Todavia algoritimo 1 não será interferido por esse problema !
+# 
+# 
+# ### Algorithm 1. Problema de Classificação ! usaremos o dataset original.
+# 
+# ### Algorithm 2. Problema de Regressão ! Usaremos uma variação do dataset. Onde retiramos os problemas na coluna de Production!
 
-# In[43]:
+# # Model 1 - Classification Problem (Decision Tree Classifier)
+# 
+# - Algoritmo 1:  Objetivo: descobrir a CROP (Cultivo). A partir de 3 dados aleatórios (AREA, STATE, DISTRICT) inseridos pelo usuário, seja calculado o resultado de CROP
+
+# In[ ]:
 
 
 # Select variables (Features)
@@ -215,11 +228,7 @@ X1 = india_crop_gdp_1997_2015[['Area', 'District_Name', 'Season']]
 y1 = india_crop_gdp_1997_2015['Crop']
 
 
-# # Model 1 - Classification (Decision Tree Classifier)
-# 
-# - Algoritmo 1:  Objetivo: descobrir a CROP (Cultivo). A partir de 3 dados aleatórios (AREA, STATE, DISTRICT) inseridos pelo usuário, seja calculado o resultado de CROP
-
-# In[44]:
+# In[ ]:
 
 
 # Split Train and Test
@@ -230,11 +239,11 @@ x1_train, x1_test, y1_train, y1_test = train_test_split(X1, y1,
                                                     test_size = 0.15,
                                                    random_state = 42)
 
-model1= DecisionTreeClassifier(random_state=42)
+model1 = DecisionTreeClassifier(random_state=42)
 model1.fit(x1_train, y1_train)
 
 
-# In[45]:
+# In[ ]:
 
 
 # Predict and calculate scores
@@ -261,12 +270,6 @@ print('F1-Score: ', f1)
 # In[ ]:
 
 
-
-
-
-# In[46]:
-
-
 #------   Saving the model with pickle -----------------
 # Define o nome do arquivo em disco que irá guardar o nosso modelo
 filename = 'model_1_FindCrop.sav'
@@ -274,77 +277,146 @@ filename = 'model_1_FindCrop.sav'
 pickle.dump(model1, open(path+filename, 'wb'))
 
 
-# # Model 2 - Regression (Decision Tree Regressor)
+# # Model 2 - Regression Linear !
 # 
 # - Algoritmo 2: Objetivo: descobrir a PRODUCTION (Produção agrícola de um cultivo). A partir de 3 dados aleatórios (AREA, CROP e GDP) inseridos pelo usuário, seja calculado o resultado de PRODUCTION
 
-# In[47]:
+# In[ ]:
 
 
-# Select variables (Features)
-india_crop_gdp_1997_2015_2 = india_crop_gdp_1997_2015[india_crop_gdp_1997_2015['Production'] != '=']
+#Retiro os itens com erro na produção
+# Existem produção que possuem o valor =
+list1 = (list(india_crop_gdp_1997_2015['Crop'].value_counts().index))
+df = india_crop_gdp_1997_2015[india_crop_gdp_1997_2015['Production'] != '=']
+# Transformo produção em float
+df['Production']=df['Production'].astype(float)
 
-x2 = india_crop_gdp_1997_2015_2[['Area', 'Crop', 'GDP']]
-y2 = india_crop_gdp_1997_2015_2['Production']
+#Pego apenas valores de produção com Valores maiores que zero.
+#Pois algum erro deve ter acontecido e encheu nosso dataframe de zero.
+#Prejudicando as predições.
 
+df2 = df.query('Production > 0')
 
-# In[48]:
+list2 = list(df2['Crop'].value_counts().index)
+list2=sorted(list2)
 
+diferenca = [x for x in list1 if x not in list2]
+#[x for x in list1 if ]
 
-# Split Train and Test
-x2_train, x2_test, y2_train, y2_test = train_test_split(x2, y2, 
-                                                    test_size = 0.30,
-                                                   random_state = 42)
-
-
-# In[49]:
-
-
-# Train the Decision Tree Regressor
-from sklearn.tree import DecisionTreeRegressor
-
-model2 = DecisionTreeRegressor(random_state=42)
-model2.fit(x2_train, y2_train)
+#lista_final
+print( 'Os elementos', sorted(diferenca), 'não possuem dados suficientes para realizar a projeção !')
 
 
-# In[50]:
+# In[ ]:
 
 
-# Define o nome do arquivo em disco que irá guardar o nosso modelo
-model_2_product = 'model2_P.sav'
-# salva o modelo no disco
-pickle.dump(model2, open(path+model_2_product, 'wb'))
+#india_crop_gdp_1997_2015.query("Crop == 1")
 
 
-# In[51]:
+# ### Visualizando os dados:
+
+# ### Separando os dados por grupos de Cultivo. Faz sentido analizar a produção de banana com banana, maça com maça, etc.
+
+# In[ ]:
 
 
-# Calculate the metrics
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
+a=list(df2.groupby('Crop'))
+len(a)
+
+
+# ### Parece existir uma linearidade dos dados para Crops individuais !
+
+# In[ ]:
+
+
+RelationItensBackUp = RelationItens
+for k in diferenca:
+    RelationItens['Crop'].pop(k, None)
+
+
+# In[ ]:
+
+
+cropList = [RelationItens['Crop'][i] for i in list2]
+
+
+# ### Vamos Aplicar uma regressão linear nos dados desagrupados!
+
+# In[ ]:
+
+
 from sklearn.metrics import r2_score
-from sklearn.metrics import median_absolute_error
+
+def productionML(x2,y2,crop):
+    '''
+    Função que tem objetivo de aplicar uma Regressão Linear nos dados
+    e salvar o arquivo como o pickle'''
+    # Split Train and Test
+    x2_train, x2_test, y2_train, y2_test = train_test_split(x2, y2, 
+                                                            test_size = 0.30,
+                                                           random_state = 42)
+    
+    from sklearn.linear_model import LinearRegression
+
+    #from sklearn.linear_model import LogisticRegression
+    model2 = LinearRegression()
+
+    model2.fit(x2_train, y2_train)
+    y2_pred = model2.predict(x2_test)
+    r2 = r2_score(y2_test, y2_pred)
+    #- R-squared (Coefficient of determination) represents the coefficient of how well the values fit compared to the original values.
+    # The value from 0 to 1 interpreted as percentages. The higher the value is, the better the model is.
+    #model_2_product = 'model2_P_cropEqual'+str(crop)+'.sav'
+    
+    #pickle.dump(model2, open(path+model_2_product, 'wb'))
+    
+    #print("'{}' : pickle.load(open('static/modelos/{}', 'rb')),".format(str(crop),(model_2_product)))
+    # salva o modelo no disco
+    
+    return (model2)
 
 
-y2_pred = model2.predict(x2_test)
-mae = mean_absolute_error(y2_test, y2_pred)
-mse = mean_squared_error(y2_test, y2_pred)
-r2 = r2_score(y2_test, y2_pred)
-mad = median_absolute_error(y2_test, y2_pred)
-print('MAE: ', mae)
-print('MSE: ', mse)
-print('r2: ', r2)
-print('MAD: ', mad)
+# In[ ]:
+
+
+trainCols = ['Area',  'Crop']
+targetCols = ['Production']
+
+x2=df2[trainCols].values
+y2=df2[targetCols].values
+
+
+# In[ ]:
+
+
+modeloT = productionML(x2,y2,'')
+## R^2 de 0.003 um numero bem baixo. Nosso R-squared é um coeficiente que representa o quão bem 
+# nossos valores estão fitando os dados originais possuindo um intervalo de valores de 0 a 1 para expressar isso.
+# Quanto mais proximo de 1. melhor nossa regressão linear é.
+# Nesse caso, quase 0 de score, representa o pior dos cenários.
+
+
+# In[ ]:
+
+
+# Fazendo um modelo de regressão linear para cada crop individual
+trainCols = ['Area']
+targetCols = ['Production']
+modelVet = {}
+for i in range(len(a)):
+    modelVet[a[i][0]] = productionML(a[i][1][trainCols],
+                              a[i][1][targetCols],a[i][0])
+
+
+# In[ ]:
+
+
+model_2_product = 'model2_P_cropEqual_Final.sav'
+pickle.dump(modelVet, open(path+model_2_product, 'wb'))
 
 
 # Metricas:
 # - MAE (Mean absolute error) represents the difference between the original and predicted values extracted by averaged the absolute difference over the data set.
 # - MSE (Mean Squared Error) represents the difference between the original and predicted values extracted by squared the average difference over the data set.
 # - RMSE (Root Mean Squared Error) is the error rate by the square root of MSE.
-# - R-squared (Coefficient of determination) represents the coefficient of how well the values fit compared to the original values. The value from 0 to 1 interpreted as percentages. The higher the value is, the better the model is.
-
-# In[ ]:
-
-
-
-
+# 
